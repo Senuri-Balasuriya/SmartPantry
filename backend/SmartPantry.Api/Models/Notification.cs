9bd3cd80e61ab2bfe62a2c41f5321b52d7249936
senuri balasooriya <senuribalasuriya@gmail.com>
2025-12-03 20:25:19 +0530

    create models

--- backend/SmartPantry.Api/Models/Notification.cs
+++ backend/SmartPantry.Api/Models/Notification.cs
@@ -1,6 +1,17 @@
-﻿namespace SmartPantry.Api.Models
+﻿using System;
+
+namespace SmartPantry.Models
 {
     public class Notification
     {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // FK
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
--- backend/SmartPantry.Api/Models/SmartShoppingSuggestion.cs
+++ backend/SmartPantry.Api/Models/SmartShoppingSuggestion.cs
@@ -1,6 +1,17 @@
-﻿namespace SmartPantry.Api.Models
+﻿using System;
+
+namespace SmartPantry.Models
 {
     public class SmartShoppingSuggestion
     {
+        public int Id { get; set; }
+
+        public string SuggestedItem { get; set; }
+        public int RecommendedQuantity { get; set; }
+        public DateTime SuggestedDate { get; set; }
+
+        // FK
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
